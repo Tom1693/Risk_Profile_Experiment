c77cd75e09c2761cc793903b283e0b50e5e37741
Tom1693 <tom_bridgwater@msn.com>
2019-07-18 15:34:37 +0100

    Robot finished with camera and hazard lines

---
+++ Assets/Scripts/RobotController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class RobotController : MonoBehaviour
+{
+    [SerializeField] Transform goal;
+    [SerializeField] AudioSource engineSound;
+
+    public Camera cam;
+    public NavMeshAgent agent;
+    public Path_Loader waypointList;
+
+    public Renderer trackLeft;
+    public Renderer trackRight;
+
+
+    private Vector3 previousPosition;
+    public float curSpeed;
+    public float prevSpeed;
+
+    int gateCounter = 0;
+
+
+    Vector3[,] fullWaypointsList = new Vector3[10,3];
+    Vector3 currentWaypoint = new Vector3();
+
+    void Start()
+    {
+        fullWaypointsList = waypointList.fullWaypointsList;
+    }
+
+
+    void Update()
+    {
+        curSpeed = GetRobotSpeed();
+        PlayEngineNoise(curSpeed);
+        AnimateTracks();
+
+        if (curSpeed == 0)
+        {
+            GetDecision();
+            transform.LookAt(goal); //look forwards
+        }
+
+        if (gateCounter >= 1) // if we're not at the beginning of the level
+        {
+            MoveToWaypoint();
+        }
+
+    }
+
+    private void AnimateTracks()
+    {
+        float offset = Time.time * curSpeed;
+
+        trackLeft.material.SetTextureOffset("_MainTex", new Vector2(offset, 0));
+        trackRight.material.SetTextureOffset("_MainTex", new Vector2(offset, 0));
+    }
+
+    private float GetRobotSpeed()
+    {
+        Vector3 curMove = transform.position - previousPosition;
+        curSpeed = curMove.magnitude / Time.deltaTime;
+        previousPosition = transform.position;
+
+        return curSpeed;
+    }
+
+    void PlayEngineNoise(float curSpeed)
+    {
+        engineSound.volume = 0.1f;
+        engineSound.pitch = 1f * curSpeed / 3;
+
+        if (curSpeed == 0)
+        {
+            engineSound.volume = 0.05f;
+            engineSound.pitch = 1f;
+        }
+    }
+
+    private void MoveToWaypoint()
+    {
+        agent.SetDestination(currentWaypoint);
+
+
+        float dist = Vector3.Distance(currentWaypoint, transform.position);
+
+        if (dist < 1)
+        {
+            currentWaypoint = fullWaypointsList[gateCounter-1, 2];
+        }
+    }
+
+    void GetDecision()
+    {
+        if (Input.GetKeyDown("u"))
+        {
+            currentWaypoint = fullWaypointsList[gateCounter, 0];
+
+            gateCounter++;
+        }
+        else if (Input.GetKeyDown("c"))
+        {
+            currentWaypoint = fullWaypointsList[gateCounter, 1];
+
+            gateCounter++;
+        }
+    }
+
+    
+
+
+    /*void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+    }*/
+}
